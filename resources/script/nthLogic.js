14a5114c2c38281557d0345cc2cb3561a22d6250
unknown <stele1000@gmail.com>
2020-05-02 12:22:09 +0200

    echo

---
+++ resources/script/nthLogic.js
@@ -0,0 +1,6 @@
+const toOdd = $(".sectionContainer section:nth-child(odd)");
+const toEven = $(".sectionContainer section:nth-child(even)");
+
+
+toOdd.addClass("oddText");
+toEven.addClass("evenText");
