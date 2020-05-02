069349646c6151426b92b8cde466ea717bacc5d0
unknown <stele1000@gmail.com>
2020-05-02 12:37:44 +0200

    sections

--- resources/script/nthLogic.js
+++ resources/script/nthLogic.js
@@ -1,6 +1,9 @@
 const toOdd = $(".sectionContainer section:nth-child(odd)");
 const toEven = $(".sectionContainer section:nth-child(even)");
+/*const toOdd = $(".sectionContainer section:nth-child(odd) .contentContainer");
+const toEven = $(".sectionContainer section:nth-child(even) .contentContainer");*/
 
 
-toOdd.addClass("oddText");
-toEven.addClass("evenText");
+toOdd.addClass("odd");
+toEven.addClass("even");
+$(".sectionContainer section:nth-child(1)").css("margin-top","30px");
