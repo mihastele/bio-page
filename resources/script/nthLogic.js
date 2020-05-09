b9a9d140b21c05effeb7e7ddf48e63ba70859280
unknown <stele1000@gmail.com>
2020-05-09 20:04:33 +0200

    v

--- resources/script/nthLogic.js
+++ resources/script/nthLogic.js
@@ -60,9 +60,11 @@ liPairsWork.forEach(liPair => {
 
 
 var liPairsFree = [["Professional website projects (including this profile page)", "2020-"],
+["Professional collaboration in projects", "2020-"],
 ["Home made loudspeaker", "2020"], ["Bladeless fan with 3D printer", "2019"],
 ["Personal social media application", "2019"],
-["Tower defense Android application with LibGDX", "2017"]];
+["Tower defense Android application with LibGDX", "2017"],
+["Contribute to open source in free time", "2017-"]];
 
 liPairsFree.forEach(liPair => {
     var struct = `<li style="margin-top: 20px">
